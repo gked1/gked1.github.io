3f60d573e6523713861d691f5c535d5f8ab9cfc4
gked1 <41132538+gked1@users.noreply.github.com>
2019-02-20 23:04:42 +0100

    Update script.js

--- js_exercises/page2/js/script.js
+++ js_exercises/page2/js/script.js
@@ -1,1 +1,45 @@
-var el = document.getElementById("btn");el.addEventListener("click", getLongWord, false);function getLongWord() {	var b = document.getElementById("sentence").value,	    c = b.replace(/[.!?,;:\[\]#_'"()-]/gi, '').replace(/[–„”]/gi, '').replace(/[\r\n]+/g, ' '),        d = c.split(" "),	    senArr = d.filter(function(item, pos) { return d.indexOf(item) == pos; } ),        senArrDig = [],	    senArrDig2 = [],		max = 0,        maxIdx = 0,		join = '',		msg;    for (var i=0; i<senArr.length; i++) {        var leng = senArr[i].length;        senArrDig.push(leng);    }    for (var i=0; i<senArrDig.length; i++) {        if (senArrDig[i] > max)          max = senArrDig[i];     }    	for (var i=0; i<senArrDig.length; i++) {        if (senArrDig[i] == max) 		  senArrDig2.push(i);    }	    for (var i=0; i<senArr.length; i++) {      for (var j=0; j<senArrDig2.length; j++) {        if (senArrDig2[j]==i)		  join += senArr[senArrDig2[j]] + ' , ';       }    }    join = join.substring(0, join.length - 3);			msg = '<h2>The longest words are: </h2><p id="result">' + join + '</p><h2> with number of characters:</h2><p>' + max + '</p>';    var el = document.getElementById('info');    el.innerHTML = msg;}
+
+var el = document.getElementById("btn");
+el.addEventListener("click", getLongWord, false);
+
+function getLongWord() {
+  var b = document.getElementById("sentence").value,
+      c = b.replace(/[.!?,;:\[\]#_'"()-]/gi, '').replace(/[–„”]/gi, '').replace(/[\r\n]+/g, ' '),
+      d = c.split(" "),
+      senArr = d.filter(function(item, pos) { return d.indexOf(item) == pos; } ),
+      senArrDig = [],
+      senArrDig2 = [],
+      max = 0,
+      maxIdx = 0,
+      join = '',
+      msg;
+
+  for (var i=0; i<senArr.length; i++) {
+    var leng = senArr[i].length;
+    senArrDig.push(leng);
+  }
+  
+  for (var i=0; i<senArrDig.length; i++) {
+    if (senArrDig[i] > max)
+    max = senArrDig[i]; 
+  }
+  
+  for (var i=0; i<senArrDig.length; i++) {
+    if (senArrDig[i] == max)
+      senArrDig2.push(i);
+  }
+  
+  for (var i=0; i<senArr.length; i++) {
+    for (var j=0; j<senArrDig2.length; j++) {
+      if (senArrDig2[j]==i)
+        join += senArr[senArrDig2[j]] + ' , '; 
+    }
+  }
+  
+  join = join.substring(0, join.length - 3);
+  	
+  msg = '<h2>The longest words are: </h2><p id="result">' + join + '</p><h2> with number of characters:</h2><p>' + max + '</p>';
+  
+  var el = document.getElementById('info');
+  el.innerHTML = msg;
+}
