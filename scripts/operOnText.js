dfb39085076d56680918d07ce040fe6336dcda5d
Grzegorz (Greg) Kędzior <41132538+gked1@users.noreply.github.com>
2020-12-02 20:09:21 +0100

    refactoring

--- scripts/operOnText.js
+++ scripts/operOnText.js
@@ -1,48 +1,50 @@
-document.getElementById("btn").addEventListener("click", getLongWord, false);
+document.getElementById("btn").addEventListener("click", getTheLongestWord, false);
 
-function getLongWord() {
-  var str1 = document.getElementById("sentence").value.replace(/[^0-9a-z]/gi, ' ').trim();
+function getTheLongestWord() {
+  var shapedInputString = document.getElementById("sentence").value.replace(/[^0-9a-z]/gi, ' ').trim();
    
-  if (typeof str1 !== 'undefined' && str1.length > 0) {
+  if (typeof shapedInputString !== 'undefined' && shapedInputString.length > 0) {
 	  
-  var  d = str1.split(' '),
-       senArr = d.filter(function(item, pos) { return d.indexOf(item) == pos; } ),
-       senArrDig = [],
-       senArrDig2 = [],
-       max = 0,
-       maxIdx = 0,
-       join = '',
-       msg;
-  //clarify length of every word in sentence
-  for (var i=0; i<senArr.length; i++) {
-    var leng = senArr[i].length;
-    senArrDig.push(leng);
-  }
-  //finding word that has max length
-  for (var i=0; i<senArrDig.length; i++) {
-    if (senArrDig[i] > max)
-      max = senArrDig[i]; 
-  }
-  //handling case when word with max length occurs more than 1 time
-  for (var i=0; i<senArrDig.length; i++) {
-    if (senArrDig[i] == max)
-      senArrDig2.push(i);
-  }
-  
-  for (var i=0; i<senArr.length; i++) {
-    for (var j=0; j<senArrDig2.length; j++) {
-      if (senArrDig2[j]==i)
-        join += senArr[senArrDig2[j]] + ' , '; 
+    var splitedString  = shapedInputString.split(' '),
+        uniqueOnlyString = splitedString.filter(function(item, pos) { 
+          return splitedString.indexOf(item) == pos; 
+        }),
+        arrStringLength = [],
+        arrStringLengthMaxValHasMultipleOccur = [],
+        intStringMaxLength = 0,
+        intStringMaxLengthIndex = 0,
+        combinedString = '',
+        msgPrintToUser;
+    //push length of every word to array
+    for (var i=0; i<uniqueOnlyString.length; i++) {
+      var leng = uniqueOnlyString[i].length;
+      arrStringLength.push(leng);
     }
-  }
-  
-  join = join.substring(0, join.length - 3);
-  msg = '<h2>The longest words are: </h2><p id="result">' + join + '</p><h2> with number of characters:</h2><p>' + max + '</p>';
-  document.getElementById('info').innerHTML = msg;   
+    //finding word that has max length
+    for (var i=0; i<arrStringLength.length; i++) {
+      if (arrStringLength[i] > intStringMaxLength)
+        intStringMaxLength = arrStringLength[i]; 
+    }
+    //handling case when they are more then 1 word with max length
+    for (var i=0; i<arrStringLength.length; i++) {
+      if (arrStringLength[i] == intStringMaxLength)
+        arrStringLengthMaxValHasMultipleOccur.push(i);
+    }
+    
+    for (var i=0; i<uniqueOnlyString.length; i++) {
+      for (var j=0; j<arrStringLengthMaxValHasMultipleOccur.length; j++) {
+        if (arrStringLengthMaxValHasMultipleOccur[j]==i)
+          combinedString += uniqueOnlyString[arrStringLengthMaxValHasMultipleOccur[j]] + ' , '; 
+      }
+    }
+    
+    combinedString = combinedString.substring(0, combinedString.length - 3);
+    msgPrintToUser = '<h2>The longest words are: </h2><p id="result">' + combinedString + '</p><h2> with number of characters:</h2><p>' + intStringMaxLength + '</p>';
+    document.getElementById('info').innerHTML = msgPrintToUser;   
 	  
   } else {
-    msg = '<h2>Enter any text in text field</h2>';
-    document.getElementById('info').innerHTML = msg;
+    msgPrintToUser = '<h2>Enter any text in text field</h2>';
+    document.getElementById('info').innerHTML = msgPrintToUser;
   };
   
 }
